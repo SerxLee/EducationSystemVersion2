2481416a1ac10e6b9dbb27c52a02f9b621cfb39b
serxlee <serx.lee@gmail.com>
2016-05-23 09:03:06 +0800

    Initial Commit

---
+++ EducationSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EducationSystem
+//
+//  Created by Serx on 16/5/23.
+//  Copyright © 2016年 Serx.Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
